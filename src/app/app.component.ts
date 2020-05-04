ca86fb486fa2c0dc52cb4d39c1b9703e8fe04e24
jianzhong.rong <tony.tiger.rong@gmail.com>
2020-05-04 15:46:05 -0300

    18 create a Reactive Form (1) [formGroup]="formGroupVariable" (2) formControlName/formGroupName="formControl/formGroupName variabel" 19/20 setValue / patchValue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'reactive-forms';
+  registrationForm = new FormGroup({
+    userName: new FormControl(''),
+    password: new FormControl(''),
+    confirmPassword: new FormControl(''),
+    address: new FormGroup({
+      city: new FormControl(''),
+      state: new FormControl(''),
+      postCode: new FormControl('')
+    })
+  });
+
+  loadData(){
+    // this.registrationForm.setValue({
+      this.registrationForm.patchValue({
+      // userName: "tony rong",
+      password: "1234",
+      confirmPassword: "1234",
+      address: {
+        city: "bedford",
+        state: "NS",
+        postCode: "B3B4T0"
+      }
+    });
+  }
 }
